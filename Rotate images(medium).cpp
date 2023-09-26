15bcc684bdee6a28be922c6a85924043c1ad472d
Anisha Singh <142076252+Anisha-singh2207@users.noreply.github.com>
2023-09-26 21:52:54 +0530

    Add files via upload

---
+++ Rotate images(medium).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n=matrix.size();
+      //lets find first transpose
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<matrix[i].size();j++){
+                swap(matrix[i][j],matrix[j][i]);
+
+            }
+        }
+
+        //now reverse the matrix
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
+    }
+};
